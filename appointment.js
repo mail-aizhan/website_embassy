1676375318a91ce62ccd88aa1c8a1ba93d2c76db
mail-aizhan <65739091+mail-aizhan@users.noreply.github.com>
2021-04-17 03:04:45 +0300

    some changes

---
+++ appointment.js
@@ -0,0 +1,14 @@
+var appontForm =document.getElementById("appointForm");
+var firstName =document.getElementById("firstName");
+var surName =document.getElementById("lastName");
+var passId =document.getElementById("passportId");
+var phoneNumber =document.getElementById("phoneNumber");
+var email =document.getElementById("email");
+var date =document.getElementById("date");
+var inputTime =document.getElementById("inputTime");
+var submitButton =document.getElementById("submitButton");
+
+
+function afterSubmit(e){
+    e.pre
+}
